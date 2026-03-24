761db306243a6094ce28fef64bbd05dd73c72aa0
sanjukumar2006 <sanju2006kumar9@gmail.com>
2026-03-24 11:04:54 +0530

    training the model to predict using the given csv data ,to  create the model.pkl

--- model/train_model.py
+++ model/train_model.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import pickle
 
-data = pd.read_csv('../data/students.csv')
+data = pd.read_csv(r'E:\Sanju\VIT college\sem 2\AI and ml\vityarthi_project\data\student.csv')
 
 X = data[['study_hours','attendance','previous_marks','assignments','internal_test']]
 y = data['final_marks']
@@ -10,6 +10,6 @@ y = data['final_marks']
 model = LinearRegression()
 model.fit(X,y)
 
-pickle.dump(model, open('model.pkl','wb'))
+pickle.dump(model, open(r'E:\Sanju\VIT college\sem 2\AI and ml\vityarthi_project\model\model.pkl','wb'))
 
 print("Model trained and saved!")
