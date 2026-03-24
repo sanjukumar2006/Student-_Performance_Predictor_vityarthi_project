98358d2e1d02d5ca4b50a2b216ee35d64dcf654b
sanjukumar2006 <sanju2006kumar9@gmail.com>
2026-03-24 10:57:13 +0530

    creating a prediction app

---
+++ app/predictor.py
@@ -0,0 +1,8 @@
+
+
+study_hours = float(input("Study hours: "))
+attendance = float(input("Attendance: "))
+previous = float(input("Previous marks: "))
+assignments = float(input("Assignments: "))
+internal = float(input("Internal test: "))
+
